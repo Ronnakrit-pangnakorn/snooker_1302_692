b515ca413d3278b27821187053904d66c7a5da58
Ronnakrit-pangnakorn <thanaphat.pangnakorn@gmail.com>
2026-07-18 14:13:39 +0700

    test commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,33 @@
+using System.Threading;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private int n = 0;
+
+    private float timer = 0f;
+     void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        n++;
+
+        if (timer >= 1f)
+        {
+            Debug.Log(n);
+            timer = 0f;
+            n = 0;
+
+        }
+    }
+}
